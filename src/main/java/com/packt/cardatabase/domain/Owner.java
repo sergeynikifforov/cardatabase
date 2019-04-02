4d4a2bddd52775adb012d4b2292ead6af8007913
sergeynikiforov761 <sergeynikiforov761@gmail.com>
2019-04-02 14:29:00 +0300

    initial state of database

---
+++ src/main/java/com/packt/cardatabase/domain/Owner.java
@@ -0,0 +1,43 @@
+package com.packt.cardatabase.domain;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Owner {
+    //fields for Owner table
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long ownerid;
+    private String firstname, lastname;
+
+    // One to many link with Car table
+    @OneToMany(cascade = CascadeType.ALL, mappedBy="owner")
+    private List<Car> cars;
+
+    public Owner(){}
+
+    public Owner(String firstname, String lastname){
+        super();
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+    public long getOwnerid() {
+        return ownerid;
+    }
+    public void setOwnerid(long ownerid) {
+        this.ownerid = ownerid;
+    }
+    public String getFirstname(){
+        return firstname;
+    }
+    public void setFirstname(String firstname){
+        this.firstname = firstname;
+    }
+    public String getLastname(){
+        return lastname;
+    }
+    public void setLastname(){
+        this.lastname = lastname;
+    }
+}
